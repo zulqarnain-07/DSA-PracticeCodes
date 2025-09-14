8208dd63108bf5e8bc15fde21460cd184dc97fdc
Zulqarnain <thatshigh78@gmail.com>
2025-09-14 22:21:01 +0500

    Add files via upload

---
+++ Week 2/1.cs
@@ -0,0 +1,29 @@
+﻿//Write a code which prints the following series:
+//     2   4   8 - - - -n
+
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+class MatrixOperations
+{
+    static void Main()
+    {
+
+        Console.Write("Enter the limit (n): ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Series:");
+        printsequence(2, n);
+    }
+    public static void printsequence(int num , int n)
+    {
+       if(num> n)
+        {
+            return;
+        }
+        Console.WriteLine(num + " ");
+        printsequence(num*2 , num);
+    }
+
+
+}
---
+++ Week 2/2.cs
@@ -0,0 +1,24 @@
+﻿//Write a program which calculates the square of a number using 
+//odd number series implemented with the help of recursion concept.
+
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+class MatrixOperations
+{
+    static void Main()
+    {
+
+        Console.Write("Enter a number: ");
+        int num = int.Parse(Console.ReadLine());
+
+        int result = SquareUsingOdd(num);
+        Console.WriteLine($"The square of {num} is: {result}");
+    }
+
+    public static int SquareUsingOdd(int n , int odd = 1) { 
+        if(n == 1) {  return 1; }
+        return odd + SquareUsingOdd(n - 1, odd + 2);
+    }
+
+}
