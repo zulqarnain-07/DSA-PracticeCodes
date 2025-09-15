e1fc241d1c948eaa1b3d95650b6daff21c3e1471
Zulqarnain <thatshigh78@gmail.com>
2025-09-15 21:00:23 +0500

    Add files via upload

---
+++ Week 2/3.cs
@@ -0,0 +1,23 @@
+﻿//Write a program which takes input of an integer number and returns the sum of all numbers.
+//i.e., if input is 3453 then the output should be 15 (3+4+5+3).
+
+
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+class MatrixOperations
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter Number : ");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine(sumDigits(n));
+      
+    }
+
+    public static int sumDigits(int n)
+    {
+        if (n == 0) { return 0; }
+        return (n % 10) + sumDigits(n / 10);
+    }
+}
---
+++ Week 2/4.cs
@@ -0,0 +1,39 @@
+﻿//Calculation of number of moves for N number of disk in
+//Tower of Hanoi problem using recursion.
+
+
+
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+class MatrixOperations
+{
+  
+    
+        public static void Main(String[] args)
+    {
+        int n = 4;
+        towerOfHanoi(n, 'A', 'C', 'B'); 
+
+        int totalMoves = (int)Math.Pow(2, n) - 1;
+        Console.WriteLine("Total moves required: " + totalMoves + "\n");
+
+    }
+
+
+    static void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod)
+    {
+
+        if (n == 1)
+        {
+            Console.WriteLine("Move disk 1 from rod " +
+            from_rod + " to rod " + to_rod);
+            return;
+        }
+        towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+        Console.WriteLine("Move disk " + n + " from rod " +
+        from_rod + " to rod " + to_rod);
+        towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
+    }
+
+}
